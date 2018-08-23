44b591d92c6350cf5824d8c952ce60e6c16d2c9d
Mark Taylor <mark@embedcreativity.com>
2018-08-22 23:18:03 -0400

    rfcomm watch is so much better than rfcomm listen

--- launchBT_SP.sh
+++ launchBT_SP.sh
@@ -2,10 +2,13 @@
 
 echo "Restarting bluetooth service..."
 sudo systemctl restart bluetooth
-sleep 5
+sleep 10
 echo "Starting rtl hci script..."
 sudo ./start_bt.sh
 sleep 10
+echo "Checking hciconfig -a... (Ctrl-C if needed)"
+hciconfig -a
+sleep 5
 echo "Calling hciconfig hci0 up..."
 sudo /bin/hciconfig hci0 up
 sleep 5
@@ -17,7 +20,7 @@ echo "/bin/hciconfig hci0 piscan..."
 sudo /bin/hciconfig hci0 piscan
 echo "/usr/bin/sdptool add --channel=22 SP..."
 sudo /usr/bin/sdptool add --channel=22 SP
-echo "Launching SSP listener..."
-sudo rfcomm listen /dev/rfcomm0 22
+echo "Launching SSP watcher..."
+sudo rfcomm watch /dev/rfcomm0 22
 
 exit 0
